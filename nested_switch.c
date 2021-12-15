787c31aebfd4895dfdd3e5aad38eef4bfcefcbc0
sairajesh069 <69496363+sairajesh069@users.noreply.github.com>
2021-12-15 19:13:01 +0530

    Add files via upload

---
+++ nested_switch.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,choice,ac,rc,bc;
+	printf("Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	printf("1. Arithmetic\n2. Relational\n3. Bitwise\nEnter your choice:");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+			printf("1. +\n2. -\n3. *\n4. /\n5. %%\nEnter your choice:");
+			scanf("%d",&ac);
+			switch(ac)
+			{
+				case 1:
+					printf("%d + %d = %d",a,b,a+b);
+					break;
+				case 2:
+					printf("%d - %d = %d",a,b,a-b);
+					break;
+				case 3:
+					printf("%d * %d = %d",a,b,a*b);
+					break;
+				case 4:
+					printf("%d / %d = %d",a,b,a/b);
+					break;
+				case 5:
+					printf("%d %% %d = %d",a,b,a%b);
+					break;
+				default:
+					printf("Enter valid input");
+					break;
+			}
+			break;
+		case 2:
+			printf("1. >\n2. >=\n3. <\n4. <=\n5. ==\n6. !=\nEnter your choice:");
+			scanf("%d",&rc);
+			switch(rc)
+			{
+				case 1:
+					printf("%d > %d = %d",a,b,a>b);
+					break;
+				case 2:
+					printf("%d >= %d = %d",a,b,a>=b);
+					break;
+				case 3:
+					printf("%d < %d = %d",a,b,a<b);
+					break;
+				case 4:
+					printf("%d <= %d = %d",a,b,a<=b);
+					break;
+				case 5:
+					printf("%d == %d = %d",a,b,a==b);
+					break;
+				case 6:
+					printf("%d != %d = %d",a,b,a!=b);
+					break;
+				default:
+					printf("Enter valid input");
+					break;
+			}
+			break;
+		case 3:
+			printf("1. &\n2. |\n3. ^\nEnter your choice:");
+			scanf("%d",&bc);
+			switch(bc)
+			{
+				case 1:
+					printf("%d & %d = %d",a,b,a&b);
+					break;
+				case 2:
+					printf("%d | %d = %d",a,b,a|b);
+					break;
+				case 3:
+					printf("%d ^ %d = %d",a,b,a^b);
+					break;
+		    	default:
+					printf("Enter valid input");
+					break;
+			}
+			break;
+		default:
+			printf("Enter valid input");
+			break;
+	}
+}
